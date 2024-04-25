a41f0574b4af4da2f90a27cf614e185b571f74cb
Jerry Lee <cosmos53076@163.com>
2024-04-25 21:59:52 +0800

    Adds utility methods SingleUtility.GenericParse and SingleUtility.GenericTryParse.

--- src/ReSharp.Extensions/System/SingleUtility.cs
+++ src/ReSharp.Extensions/System/SingleUtility.cs
@@ -8,8 +8,16 @@ namespace ReSharp.Extensions
     public static class SingleUtility
     {
         /// <summary>
-        /// Converts the string representation of a number to its single-precision floating-point number equivalent with number style <see cref="NumberStyles.Any"/>
-        /// and format provider <see cref="CultureInfo.InvariantCulture"/>. A return value indicates whether the conversion succeeded or failed.
+        /// Converts the string representation of a number in style of <see cref="NumberStyles.Any"/> and <see cref="CultureInfo.InvariantCulture"/> format
+        /// to its single-precision floating-point number equivalent.
+        /// </summary>
+        /// <param name="s">A string that contains a number to convert.</param>
+        /// <returns>A single-precision floating-point number equivalent to the numeric value or symbol specified in <c>s</c>. </returns>
+        public static float GenericParse(string s) => float.Parse(s, NumberStyles.Any, CultureInfo.InvariantCulture);
+        
+        /// <summary>
+        /// Converts the string representation of a number to its single-precision floating-point number equivalent in style of <see cref="NumberStyles.Any"/>
+        /// and <see cref="CultureInfo.InvariantCulture"/> format. A return value indicates whether the conversion succeeded or failed.
         /// </summary>
         /// <param name="s">A string representing a number to convert.</param>
         /// <param name="result">When this method returns, contains single-precision floating-point number equivalent to the numeric value or symbol contained in <c>s</c>,
--- test/ReSharp.Extensions.Tests/System/SingleUtilityTests.cs
+++ test/ReSharp.Extensions.Tests/System/SingleUtilityTests.cs
@@ -8,11 +8,19 @@ namespace ReSharp.Extensions.Tests
     public class SingleUtilityTests
     {
         [Test]
-        public void GenericTryParseTest1()
+        public void GenericParseTest()
         {
             Thread.CurrentThread.CurrentCulture = new CultureInfo("de-DE");
-            SingleUtility.GenericTryParse("0.123", out var value);
-            Assert.AreEqual(0.123f, value);
+            var result = SingleUtility.GenericParse("0.123");
+            Assert.AreEqual(0.123f, result);
+        }
+        
+        [Test]
+        public void GenericTryParseTest()
+        {
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("de-DE");
+            SingleUtility.GenericTryParse("0.123", out var result);
+            Assert.AreEqual(0.123f, result);
         }
     }
 }
